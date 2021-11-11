5feda332c023118fce1ec7ab095a04959436992b
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-11-11 00:40:02 -0300

    surya: refreshrate: Update refresh rate props values from Raven
          * https://android.googlesource.com/device/google/gs101/+/refs/tags/android-12.0.0_r13/device.mk#615
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: I3aa1d4a037ac3ed2b881700110ef5b73567385c1

--- refreshrate.mk
+++ refreshrate.mk
@@ -5,12 +5,15 @@ PRODUCT_PACKAGES += \
 
 # Refresh Rate Dynamic Props
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.set_idle_timer_ms=80 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.set_idle_timer_ms?=80 \
     ro.surface_flinger.set_touch_timer_ms=200 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.disable_backpressure=0 \
+    debug.sf.enable_gl_backpressure=1 \
     vendor.display.defer_fps_frame_count=4 \
     vendor.display.idle_time=0 \
     vendor.display.idle_time_inactive=0 \
@@ -26,11 +29,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.sf.use_phase_offsets_as_durations=1 \
     debug.sf.late.sf.duration=10500000 \
-    debug.sf.late.app.duration=20500000 \
-    debug.sf.early.sf.duration=16000000 \
-    debug.sf.early.app.duration=16500000 \
-    debug.sf.earlyGl.sf.duration=13500000 \
-    debug.sf.earlyGl.app.duration=21000000
+    debug.sf.late.app.duration=16600000 \
+    debug.sf.early.sf.duration=16600000 \
+    debug.sf.early.app.duration=16600000 \
+    debug.sf.earlyGl.sf.duration=16600000 \
+    debug.sf.earlyGl.app.duration=16600000
 else
 # Overlays
 PRODUCT_PACKAGES += \
@@ -42,7 +45,8 @@ PRODUCT_COPY_FILES += \
 
 # Refresh Rate Props
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.set_idle_timer_ms=80 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.set_idle_timer_ms?=80 \
     ro.surface_flinger.set_touch_timer_ms=200 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true \
